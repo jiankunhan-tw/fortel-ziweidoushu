466fcb2ad0d9c77cffd2a04f27d76561b19abadc
jiankunhan-tw <quen840404@outlook.com>
2025-06-18 20:37:22 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Calendar, CalendarType } from './calendar/calendar'
+import { Calendar, CalendarType } from './calendar/calender'
 import { defaultCalendar } from './calendar/defaultCalendar'
 import { BoardCriteria, CellsScope } from './criteria/boardCriteria'
 import { DestinyConfigBuilder } from './util/destinyConfigBuilder'
@@ -9,8 +9,8 @@ import { ConfigType, DestinyConfig, Gender } from './model/destinyConfig'
 import { Ground } from './model/ground'
 import { LifeStage } from './model/lifeStage'
 import { MajorStar } from './model/majorStar'
-import { MiniStar } from './model/miniStar'
-import { MinorStar } from './model/minorStar'
+import { MiniStar } from './model/minorStar'
+import { MinorStar } from './model/miniStar'
 import { Element, Direction, Luckiness, ShadowLight } from './model/miscEnums'
 import { Runtime } from './model/runtime'
 import { Sky } from './model/sky'
@@ -18,24 +18,35 @@ import { StarDerivative } from './model/starDerivative'
 import { Temple } from './model/temple'
 import { destinyConfigTextParser } from './util/destinyConfigTextParser'
 import { starByName, starByKey } from './util/starUtil'
-
 import type { RuntimeContext } from './model/destinyBoard'
 
+// prettier-ignore
 export {
+  // config
   DestinyConfig,
   ConfigType,
   Gender,
   DayTimeGround,
   DestinyConfigBuilder,
   destinyConfigTextParser,
+
+  // criteria
   BoardCriteria, CellsScope,
+
+  // model classes
   Sky, Ground, DestinyBoard,
   Cell, BorrowCell, Temple,
   MajorStar, MinorStar, MiniStar,
   RuntimeContext,
   StarDerivative, Runtime,
+
+  // misc enums
   LifeStage, Element, Luckiness, ShadowLight, Direction,
+
+  // calendar
   defaultCalendar, CalendarType,
+
+  // util
   starByName, starByKey
 }
 
