63c4e1eabf58cecfb85eaec13bb2f565b9af55b0
jiankunhan-tw <quen840404@outlook.com>
2025-06-18 20:19:28 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -18,31 +18,34 @@ import { StarDerivative } from './model/starDerivative'
 import { Temple } from './model/temple'
 import { destinyConfigTextParser } from './util/destinyConfigTextParser'
 import { starByName, starByKey } from './util/starUtil'
+import { lunarToSolar } from './util/lunarToSolar' // ✅ 新增這行
+
 import type { RuntimeContext } from './model/destinyBoard'
 
 // prettier-ignore
 export {
-    // config
-    DestinyConfig,
-    ConfigType,
-    Gender,
-    DayTimeGround,
-    DestinyConfigBuilder,
-    destinyConfigTextParser,
-    // criteria
-    BoardCriteria, CellsScope,
-    // model classes
-    Sky, Ground, DestinyBoard,
-    Cell, BorrowCell, Temple,
-    MajorStar, MinorStar, MiniStar,
-    RuntimeContext,
-    StarDerivative, Runtime,
-    // misc enums
-    LifeStage, Element, Luckiness, ShadowLight, Direction,
-    // calendar
-    defaultCalendar, CalendarType,
-    // util
-    starByName, starByKey
+  // config
+  DestinyConfig,
+  ConfigType,
+  Gender,
+  DayTimeGround,
+  DestinyConfigBuilder,
+  destinyConfigTextParser,
+  // criteria
+  BoardCriteria, CellsScope,
+  // model classes
+  Sky, Ground, DestinyBoard,
+  Cell, BorrowCell, Temple,
+  MajorStar, MinorStar, MiniStar,
+  RuntimeContext,
+  StarDerivative, Runtime,
+  // misc enums
+  LifeStage, Element, Luckiness, ShadowLight, Direction,
+  // calendar
+  defaultCalendar, CalendarType,
+  // util
+  starByName, starByKey,
+  lunarToSolar // ✅ 新增這行
 }
 
 export type { Calendar }
